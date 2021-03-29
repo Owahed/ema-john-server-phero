4382c8f8afb4c296df4531e2bbb1c0bbb7c36088
Owahed <owahedhossain@gmail.com>
2021-03-29 15:44:39 +0600

    root create

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ app.use(cors());
 
 const port = 5000
 
-
+app.get('/', (req,res)=>{
+    res.send('hello from db its working')
+})
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
